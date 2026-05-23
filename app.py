844551d1b70bcbcf7995f50938162605e4910c67
punith <punitiptur2@gmail.com>
2026-05-23 17:33:39 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import subprocess
+
+command = [
+    "eksctl", "create", "cluster",
+    "--name", "test",
+    "--region", "us-east-1",
+    "--node-type", "t2.medium",
+    "--nodes", "6"
+]
+
+subprocess.run(command, check=True)
